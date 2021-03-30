92b41667822503f2634ef3504189fbce04de0c36
Nitai Charan <nitaicharan@gmail.com>
2021-03-30 18:16:59 -0300

    37. Llamando a APIs usando thunks

--- src/thunk.js
+++ src/thunk.js
@@ -1,6 +1,51 @@
-export default payload => {
-    return (dispatch, getState) => {
-        const state = getState();
-        console.log(state)
+const FETCH_START = '';
+const FETCH_SUCCESS = '';
+const FETCH_ERROR = '';
+
+const fetchStart = () => ({
+    type: FETCH_START,
+});
+const fetchSuccess = payload => ({
+    type: FETCH_SUCCESS,
+    payload,
+});
+const fetchError = error => ({
+    type: FETCH_ERROR,
+    error,
+});
+
+const url = 'https://jsonplaceholder.typicode.com/users';
+
+const initialState = {
+    data: [],
+    fetching: false,
+    error: null,
+};
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case FETCH_START:
+            return { ...state, fetching: true };
+        case FETCH_SUCCESS:
+            return { ...state, data: action.payload };
+        case FETCH_ERROR:
+            return { ...state, error: action.error };
+        default:
+            return { ...state }
+    }
+}
+
+export default function initThunk(payload) {
+    return async (dispatch, getState) => {
+        dispatch(fetchStart());
+
+        try {
+            const result = await fetch(url);
+            const json = await result.json();
+            dispatch(fetchSuccess(json));
+            console.log(json);
+        } catch (error) {
+            dispatch(fetchError(error));
+        }
     };
 }
+
